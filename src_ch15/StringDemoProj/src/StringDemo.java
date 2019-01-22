2c669f0a9dbdaafe4d2b92534a2b1ef33cafc853
peterhchen <peter.hp.chen@gmail.com>
2019-01-22 13:26:40 -0800

    Create String.

---
+++ src_ch15/StringDemoProj/src/StringDemo.java
@@ -0,0 +1,7 @@
+public class StringDemo {
+    public static void main (String[] args) {
+        char [] helloArray = {'h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'};
+        String helloString = new String (helloArray);
+        System.out.println (helloString);
+    }
+}
