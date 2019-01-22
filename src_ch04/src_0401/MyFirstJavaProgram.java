e93a061304a55ff3ffd9216c322280e2874fc2c3
peterhchen <peter.hp.chen@gmail.com>
2019-01-21 19:39:47 -0800

    Basic Java

---
+++ src_ch04/src_0401/MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaProgram {
+    /* This is my first Java Program.
+       This will print 'Hello World' as output.
+     */
+    public static void main (String[] args) {
+        System.out.println ("Hello world"); // print Hello World
+    }
+}
+
---
+++ src_ch05/ClassicSingletonProj/src/ClassicSingletonDemo.java
@@ -0,0 +1,6 @@
+public class ClassicSingletonDemo {
+    public static void main(String[] args) {
+        ClassicSingleton tmp = ClassicSingleton.getInstance();
+        tmp.demoClassicSingletonMethod();
+    }
+}
